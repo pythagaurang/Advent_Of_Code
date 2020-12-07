773a7ece4ca49559cb7db4c073ce05ef774d5494
Gaurang Govekar <gaurangsg.28@gmail.com>
2020-12-07 15:44:50 +0530

    2017 d1

---
+++ Practice/2017/1/1_2.py
@@ -0,0 +1,13 @@
+import re
+
+file = open('input2.txt')
+input_list = []
+
+for line in file:
+    input_list.append(line.strip())
+
+def get_sum(num):
+    l=len(num)
+    return sum(int(num[i]) for i in range(len(num)) if num[i]==num[(i+l//2)%l])
+for num in input_list:
+    print(get_sum(num))
