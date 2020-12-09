b8bd326a1cef885a27fb06454d7cfa6b29fdb4f8
Gaurang Govekar <gaurangsg.28@gmail.com>
2020-12-09 19:52:13 +0530

    2020 day 6 part 1 refactored

--- 2020/6/6_1.py
+++ 2020/6/6_1.py
@@ -1,17 +1,15 @@
-file = open('input2.txt')
-input_list = []
-for line in file:
-    input_list.append(line.strip())
-input_list+=['']
+file = open('input1.txt')
+input_list = [line.strip() for line in file]+['']
+
 questions=set()
 count=0
+
 for line in input_list:
     if line!='':
         for letter in line:
-            print(letter)
             questions.add(letter)
-            print(questions)
     else:
         count+=len(questions)
         questions=set()
+
 print(count)
